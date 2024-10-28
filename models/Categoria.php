544aba9877b017ede35b73b246332cd4af4fb155
Ernesto Ramón López <ernestorlopez@hotmail.com.ar>
2024-10-28 11:11:53 -0300

    Primeras configuraciones
    Se agrega archivo de conexión a BD

---
+++ models/Categoria.php
@@ -0,0 +1,13 @@
+<?php
+    class Categoria extends Conectar{
+        public function get_categoria(){
+            $conectar = parent::conexion();
+            parent::set_names();
+            $sql = "SELECT * FROM categorias";
+            $sql = $conectar->prepare($sql);
+            $sql -> execute();
+            return $resultado = $sql -> fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+
+?>
